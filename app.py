e065e04ba2153790649994f39dc9c4014624510e
adessoliers <alexdessoliers@yahoo.com>
2021-01-23 16:53:40 -0800

    Saving

--- app.py
+++ app.py
@@ -1,41 +1,46 @@
-import os
-from flask import Flask, jsonify, render_template, redirect, request
-import pandas as pd
-import psycopg2
-import psycopg2.extras
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+from flask import Flask, jsonify, render_template
+from sqlalchemy import create_engine
+from sqlalchemy import and_
+from sqlalchemy import or_
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import func
 
-# Database Setup
-hostname = 'localhost'
-username = 'postgres'
-password = 'password'
-database = 'emissions_db'
-connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+# Create engine
+engine = create_engine("postgresql://postgres:password@localhost/emissions_db")
 
-# Save references needed to the views
-aqi_percent = pd.read_sql_query('select * from "new_annual_aqi_percentage"', con=connection)
+# Reflect database into model
+Base = automap_base()
 
+# Reflect tables
+Base.prepare(engine, reflect=True)
 
-# FLASK SETUP
+Aqi = Base.classes.new_annual_aqi_percentage
+Geocoords = Base.classes.state_geocoord
+
+# Create session
+session = Session(engine)
+
+# Create App
 app = Flask(__name__)
 
-
-################
-#FLASK ROUTES
-################
+# Home page
 @app.route("/")
 def home():
-    return render_template ('index.html')
+    return render_template ("index.html")
 
-@app.route("/data/aqi_percent")
-def aqipercent():
-    return aqi_percent.to_json()
+@app.route("/api/v1.0/geocoords")
+def test():
+    query = session.query(Geocoords.state).all()
+    states = list(np.ravel(query))
+    return jsonify(states)
 
 
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
-
-
-
