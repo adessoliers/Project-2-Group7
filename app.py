264a8568b3f567481f218ed7f85245286133ca06
adessoliers <alexdessoliers@yahoo.com>
2021-01-24 12:15:13 -0800

    app.py flask testing

--- app.py
+++ app.py
@@ -1,43 +1,40 @@
-import numpy as np
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func, inspect
-from flask import Flask, jsonify, render_template
-from sqlalchemy import create_engine
-from sqlalchemy import and_
-from sqlalchemy import or_
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import func
+from flask import Flask, jsonify, render_template, redirect, request
+import pandas as pd
+import psycopg2
+import psycopg2.extras
 
-# Create engine
-engine = create_engine("postgresql://postgres:password@localhost/emissions_db")
+# database setup
+hostname = 'localhost'
+username = 'postgres'
+password = 'password'
+database = 'emissions_db'
+connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
 
-# Reflect database into model
-Base = automap_base()
+# Save references needed to view
+annual_aqi = pd.read_sql_query('select * from new_annual_aqi_percentage',con=connection)
+annual_generation = pd.read_sql_query('select * from new_annual_generation',con=connection)
 
-# Reflect tables
-Base.prepare(engine, reflect=True)
-
-Aqi = Base.classes.new_annual_aqi_percentage
-Geocoords = Base.classes.state_geocoord
-
-# Create session
-session = Session(engine)
-
-# Create App
+# Flask Setup
 app = Flask(__name__)
 
-# Home page
+# Flask Route
 @app.route("/")
 def home():
+    """Main page"""
     return render_template ("index.html")
 
-@app.route("/api/v1.0/geocoords")
-def test():
-    query = session.query(Geocoords.state).all()
-    states = list(np.ravel(query))
-    return jsonify(states)
+@app.route("/data/annual_aqi")
+def annualaqi():
+    return annual_aqi.to_json()
+
+@app.route("/data/annual_generation")
+def generation():
+    return annual_generation.to_json()
+
+
+
+
+
 
 
 
